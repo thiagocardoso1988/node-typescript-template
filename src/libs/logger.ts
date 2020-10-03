a07b787cbfefff73d87040c9ffaf9d772840de1d
Thiago Cardoso <intelxd@gmail.com>
2020-10-03 19:13:59 -0300

    Fix logger namespace

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -1,6 +1,6 @@
 import bunyan, { LoggerOptions } from 'bunyan';
 
-const loggerConfig: LoggerOptions = { name: 'devrockets-node-ts-template' };
+const loggerConfig: LoggerOptions = { name: 'node-ts-template' };
 const logger: bunyan = bunyan.createLogger(loggerConfig);
 
 export default logger;
