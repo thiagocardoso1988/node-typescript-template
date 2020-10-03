f57dd625c97774fe182f0ea77e6f454c11a34b4c
Thiago Cardoso <intelxd@gmail.com>
2020-10-03 19:08:33 -0300

    Fix logger namespace

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -1,6 +1,6 @@
 import bunyan, { LoggerOptions } from 'bunyan';
 
-const loggerConfig: LoggerOptions = { name: 'websocket-api' };
+const loggerConfig: LoggerOptions = { name: 'devrockets-node-ts-template' };
 const logger: bunyan = bunyan.createLogger(loggerConfig);
 
 export default logger;
